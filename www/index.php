811a680b37abb1087c8a5efd7ec93e6a0a70eced
root <root@localhost.localdomain>
2020-01-10 10:32:16 +0100

    add id field and post args in insert.php

--- www/index.php
+++ www/index.php
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 	// output data of each row
 	while($row = $result->fetch_assoc()) {
-    	echo "id: " . $row["id"]. " - Name: " . $row["nom"]. " " . $row["prenom"]. "<br>";
+    	echo "ID: " . $row["id"]. " - Name: " . $row["nom"]. " " . $row["prenom"]. "<br>";
 	}
 } else {
 	echo "0 results";
--- www/insert.php
+++ www/insert.php
@@ -4,6 +4,10 @@ $username = "user";
 $password = "test";
 $dbname = "stage";
 
+$id=$_POST['id'];
+$nom=$_POST['nom'];
+$prenom=$_POST['prenom'];
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -11,13 +15,12 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO stagiaire (nom, prenom) VALUES ('Ramadoss','Archana')";
+$sql = "INSERT INTO stagiaire (id,nom, prenom) VALUES ('$id','$nom','$prenom')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+    echo "New record $id created successfully with last name:$nom and name:$prenom";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 ?>
-
